a41d1a158774cabb2ba2eaee21c6feb60f76b8a5
lorocks <lowell.lobo@hotmail.com>
2023-10-02 22:10:09 -0400

    Modified class declaration to add constructor and control parameters

--- libs/pid/pid.hpp
+++ libs/pid/pid.hpp
@@ -3,8 +3,17 @@
 
 class PID{
     private:
-        double Kp, Ki, Kd;
+        double Kp;
+        double Ki;
+        double Kd;
+        double integral;
+        double prevError;
+        
     public:
+        PID(double kp=0, double ki=0, double kd=0) 
+        : Kp(kp), Ki(ki), Kd(kd), integral(0), prevError(0)
+        {
+        }
         const double getKp();
         const double getKi();
         const double getKd();
